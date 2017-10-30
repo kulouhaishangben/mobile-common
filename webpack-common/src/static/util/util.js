f28bc769630228996bbbfd04615c83dbd42ecf5a
lishuyang <lishuyang@mozat.com>
2017-10-30 14:21:49 +0800

    添加了两个与toast相关的方法

--- webpack-common/src/static/util/util.js
+++ webpack-common/src/static/util/util.js
@@ -67,3 +67,72 @@ export function append(parent, text) {
         parent.appendChild(text);
     }
 }
+
+
+/**
+ * myToastFn会生成一个toast，并且有淡入淡出效果（依赖zepto的fade），最后会删除这个生成的toast；
+ * @param parent [type: dom元素] [必要，一般是body或其他顶层元素]
+ * @param text [type: string] [必要，toast的文案，必须为字符串，最好不要带dom元素]
+ * @param showTime [type: number] [可选，要显示的时间，毫秒数]
+ * @param hideTime [type: number] [可选，要淡出隐藏的时间，毫秒数]
+ * 注：需要配合css样式，可引入my-toast.css文件
+ */
+export function myToastFn (parent, text, showTime = 3000, hideTime = 1000) {
+    var temp = document.createElement('div');
+    // 处理后续使用$('.my-toast')会获取到多个元素的问题，添加一个带时间戳的类名
+    var timestampClass = 'toast' + new Date().getTime()
+    //console.log(timestampClass);
+    var myToastHtml = `<div class="my-toast ${timestampClass}">
+    <div class="my-toast-text">
+        ${text}
+    </div>
+</div>`
+    temp.classList.add('my-toast')
+    temp.innerHTML = myToastHtml;
+    // 防止元素太多 进行提速
+    var frag = document.createDocumentFragment();
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    //parent.appendChild(frag);
+    parent.append(frag);
+
+    // 写动画，最后删掉整个元素
+    var $myToast = $(`.${timestampClass}`)
+    //console.log('$myToast：',$myToast) // 有个问题，就是一旦产生多个.my-toast，下面代码就会对这些.my-toast都进行处理
+    $myToast.show() // 先立即显示，再用fadeIn延迟显示的时间
+    $myToast.fadeIn(showTime, function () {
+        $myToast.fadeOut(hideTime, function () { // fadeOut必须配合fadeIn（不知道jq会不会更方便些）
+            //console.log('准备删除');
+            $myToast.remove()
+        })
+    })
+}
+
+
+/**
+ * myToastFn2处理.my-toast盒子的显示，需要一个标记变量来限制；
+ * @param myToast [type: jq对象] [必要，要显示的toast元素]
+ * @param canToast [type: object] [必要，标记变量，限制toast盒子的多次显示]
+ * @param showTime [type: number] [可选，要显示的时间，毫秒数]
+ * @param hideTime [type: number] [可选，要淡出隐藏的时间，毫秒数]
+ * 注：为了起到限制作用，canToast必须为对象，且格式为canToast = { value: true }
+ * 注：该方法就只操作一个toast，因此需要一个标记变量来限制。
+ */
+export function myToastFn2 (myToast, canToast, showTime = 3000, hideTime = 1000) {
+    //if (!canToast) { // 这种在引入该方法时会报错，因为一开始还无法找到canToast这个变量
+    if (!canToast.value) {
+        console.log('不可点击');
+        return
+    }
+    //canToast = false
+    canToast.value = false // 变为不可点击状态
+    //var showTime1 = showTime || 3000
+    myToast.show() // 先立即显示，再用fadeIn延迟显示的时间
+    myToast.fadeIn(showTime, function () {
+        myToast.fadeOut(hideTime, function () { // fadeOut必须配合fadeIn（不知道jq会不会更方便些）
+            //canToast = true
+            canToast.value = true
+        })
+    })
+}
